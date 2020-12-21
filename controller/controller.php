f322a58d0ba3be1b3c9ea02acbaecad4655cca53
Basile Ameeuw <basile.ameeuw@hotmail.com>
2020-12-21 21:58:23 +0100

    Comment controller

--- controller/controller.php
+++ controller/controller.php
@@ -4,6 +4,9 @@ require_once 'model/ProductsManager.php';
 
 class Controller
 {
+    /*
+    function postCatalog to see all the products on a page.
+    */
     function postCatalog()
     {   
         $ProductPres="yes";
@@ -11,6 +14,11 @@ class Controller
         require 'view/articles_catalogue.php';
     }
 
+    /*
+    function postProducts to show all products chose by the shoppers
+    Send $productsName (name of the products), $productsPrice (price of the products) and $total (sum of the prices of the products)
+    These were sent to the page recap
+    */
     function postProducts($nameHashPrice)
     {
         $product = new ProductsManager();
@@ -22,7 +30,11 @@ class Controller
         
         require 'view/recap.php';
     }
-
+    /*
+    function postNoProduct is set to publish again the page with all the products 
+    with a warning saying that you can't go to command without any products chosen.
+    $ProductPres let the page of products known that there isn't any articles chosen.
+    */
     function postNoProduct()
     {
         $ProductPres="no";
@@ -30,6 +42,10 @@ class Controller
         require 'view/articles_catalogue.php';
     }
 
+    /*
+    function addProducts is a backend function needed to put the new product in the DataBase which is a JSON file.
+    We enter the name ($name), the price ($price), a description ($description) and an url for an image ($image)
+    */
     function addProducts($name,$price,$description,$urlImage,$filename="./public/js/Products.json")
     {
         $product = new ProductsManager();
@@ -39,6 +55,10 @@ class Controller
         require 'view/creation_JSON.php';
     }
 
+    /*
+    function askAddProducts is just to show the view page to add a new product
+    $HasBeenHad is needed to say that no Products were supposed to be added.
+    */
     function askAddProducts()
     {
         $HasBeenHad="no";
@@ -46,6 +66,10 @@ class Controller
         require 'view/creation_JSON.php';
     }
 
+    /*
+    function addNoProduct is a function which is needed to oblige the site's owner to fil in a name and a price for an article
+    $HasBeenHad is needed to say that a product was supposed to be added but not with the good condition.
+    */
     function addNoProduct()
     {
         $HasBeenHad="noAdd";
@@ -53,11 +77,17 @@ class Controller
         require 'view/creation_JSON.php';
     }
 
+    /*
+    function askChoices shows the view to chose between adding a product or seeing the catalog with all the products.
+    */
     function askChoice()
     {
         require 'view/choice.php';
     }
 
+    /*
+    function confirm is to show to the client that the command is done.
+    */
     function confirm()
     {
         require 'view/confirmation.php';
